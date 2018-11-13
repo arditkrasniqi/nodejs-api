8d8654c41969d218cacdf1fc3c16b64440dec58a
Ardit Krasniqi <=>
2018-11-13 22:44:54 +0100

    syntax error fix

--- node-api/api/models/product.js
+++ node-api/api/models/product.js
@@ -11,7 +11,7 @@ const productSchema = mongoose.Schema({
     },
     description: {
         type: String,
-        require: true
+        required: true
     },
     productImage: {
         type: String,
